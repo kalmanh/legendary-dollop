5a581c6233626872b00b27ab7613521cae46e332
Saif Al Falah <saifalfalah@gmail.com>
2016-04-02 00:01:56 +0530

    CSS chapter. Error encountered

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,6 @@
 class CoursesController < ApplicationController
   def index
   	@search_term = 'jhu'
-  	@couses = Coursera.for(@search_term)
+  	@courses = Coursera.for(@search_term)
   end
 end
--- app/models/coursera.rb
+++ app/models/coursera.rb
@@ -1,5 +1,6 @@
 class Coursera
 	include HTTParty
+	default_options.update(verify: false)
 
 	base_uri 'https://api.coursera.org/api/catalog.v1/courses'
 	default_params fields: "smallIcon,shortDescription", q: "search"
